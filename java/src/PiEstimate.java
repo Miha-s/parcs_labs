90be41d2674fafec2a70a11086a0419171a00a5d
MihaS <mishastatnik@gmail.com>
2025-03-08 14:45:11 +0200

    adfs

--- java/src/PiEstimate.java
+++ java/src/PiEstimate.java
@@ -55,7 +55,7 @@ public class PiEstimate implements AM {
 
     public void run(AMInfo info) {
         int[] arr = (int[])info.parent.readObject();
-
+        Thread.sleep(2000);
         info.parent.write(arr);
     }
 }
